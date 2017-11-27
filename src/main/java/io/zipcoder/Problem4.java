b677cf17ac7e26e417d84c399b4cc459d89183b4
gpatelos <greg.patselas@gmail.com>
2017-11-27 15:50:57 -0500

    the kingdom is safe.

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,57 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Problem4 {
+
+
+
+    public Map<Character,Integer> createCharFrequencyMap(String input) {
+
+        char[] charArray = input.toCharArray();
+        List<Character> characterArrayList = new ArrayList<Character>();
+
+        for(char character: charArray){
+            characterArrayList.add(character);
+        }
+
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for (Character character : characterArrayList) {
+            Integer count = map.get(character);
+            map.put(character, (count == null) ? 1 : count + 1);
+        }
+        return map;
+
+    }
+
+    public static void printMap(Map<Character, Integer> map){
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.println("Key : " + entry.getKey() + " Value : "
+                    + entry.getValue());
+        }
+
+    }
+
+    public boolean isAnagramicPalindromic(String input) {
+
+        Map<Character, Integer> checkMe = new HashMap<Character, Integer>();
+        checkMe = createCharFrequencyMap(input);
+        Integer count = 0;
+        for (Map.Entry<Character, Integer> entry : checkMe.entrySet()) {
+
+            if (entry.getValue() % 2 != 0) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,72 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4Test {
+
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void getMapOfLetterFrequencyTest(){
+        Problem4 problem4 = new Problem4();
+
+        String input = "aabbbcccc";
+        Map<Character, Integer> expected = new HashMap<Character, Integer>();
+
+        expected.put('a',2);
+        expected.put('b',3);
+        expected.put('c',4);
+
+        Map<Character, Integer> actual = problem4.createCharFrequencyMap(input);
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void printMapTest(){
+
+        Problem4 problem4 = new Problem4();
+
+        String input = "cdcdcdcdeeeef";
+        Map<Character, Integer> expected = new HashMap<Character, Integer>();
+
+        Map<Character, Integer> actual = problem4.createCharFrequencyMap(input);
+        problem4.printMap(actual);
+
+    }
+
+    @Test
+    public void isAnagramicPalindromicTest(){
+
+        Problem4 problem4 = new Problem4();
+
+        String input = "cdcdcdcdeeeef";
+        boolean actual = problem4.isAnagramicPalindromic(input);
+
+        String input2 = "aaaaaaaaaaa";
+        boolean actual2 = problem4.isAnagramicPalindromic(input2);
+
+        String input3 = "qwertyuiopasdf";
+        boolean actual3 = problem4.isAnagramicPalindromic(input3);
+
+
+        Assert.assertTrue(actual);
+        Assert.assertTrue(actual2);
+        Assert.assertFalse(actual3);
+
+    }
+
+
+
+
 }
